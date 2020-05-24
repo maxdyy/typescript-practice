7467f3b5751c46b4d6099105d9f46b13fa676134
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2020-05-24 11:21:44 +0200

    types basics

---
+++ basics/app.js
@@ -0,0 +1,8 @@
+var addNumbers = function (n1, n2, logResult, message) {
+    logResult ? console.log("" + message + (n1 + n2)) : n1 + n2;
+};
+var n1 = 5;
+var n2 = 1.5;
+var logResult = true;
+var message = "Your result is: ";
+addNumbers(n1, n2, logResult, message);
